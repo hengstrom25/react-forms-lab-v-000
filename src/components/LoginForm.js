f92b9ca96172914135013316129d4cb1e5bf5ad2
Heidi Engstrom <hengstrom25@gmail.com>
2019-02-23 04:29:11 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+  
+  handleInput = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   render() {
@@ -13,13 +22,19 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text"
+            value={this.state.username}
+            onChange={this.handleInput}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" 
+            value={this.state.password}
+            onChange={this.handleInput}
+            />
           </label>
         </div>
         <div>
